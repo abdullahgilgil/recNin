f677145b03075c915615565eb946aaa1c1773079
abdullahgilgil <abdullah_gilgil@hotmail.com>
2018-12-05 23:12:35 +0000

    les-12

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+   render() {
+      return (
+         <div className="App">
+            <h1>My First React App...</h1>
+            <p>WELCOME :->></p>
+         </div>
+      );
+   }
+
+}
+
+export default App;
